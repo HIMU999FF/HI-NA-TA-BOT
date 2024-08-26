4c06b79def6fa7ac7abb598bed5b94ada4ff8049
𝄟✮⃝❤𝗛𝗜×͜×𝗠𝗨✮⃝❤𝄟 <166631945+HIMU999FF@users.noreply.github.com>
2024-08-26 22:57:31 +0600

    Create inbox.js

---
+++ scripts/inbox.js
@@ -0,0 +1,37 @@
+module.exports = {
+  config: {
+    name: "inbox",
+    aliases: ["inboxme", "in"],
+    version: "1.0",
+    author: "anchestor",
+    countDown: 10,
+    role: 0,
+    shortDescription: {
+      en: ""
+    },
+    longDescription: {
+      en: ""
+    },
+    category: "fun",
+    guide: {
+      en: ""
+    }
+  },
+  langs: {
+    en: {
+      gg: ""
+    },
+    id: {
+      gg: ""
+    }
+  },
+  onStart: async function({ api, event, args, message }) {
+    try {
+      const query = encodeURIComponent(args.join(' '));
+      message.reply("☑ |✦ 𝙷𝚘𝚘\n𝙸𝚗𝚋𝚘𝚌 𝚌𝚑𝚎𝚌𝚔 𝚍𝚎", event.threadID);
+      api.sendMessage("👀 |✦ 𝚔𝚒𝚛𝚎 𝚊𝚋𝚊𝚕", event.senderID);
+    } catch (error) {
+      console.error("Error bro: " + error);
+    }
+  }
+};
